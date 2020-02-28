3fcfec6019a497b0fce7fefafd3df971c7f01a8a
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-28 22:45:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -68,8 +68,8 @@ def reduce_to_any_true(source_array)
 
 i = 0
 while i < source_array.length do
-  return false if source_array[i] === false
+  return true if source_array[i] === true
   i+=1
 end
-return true
+return false
 end
