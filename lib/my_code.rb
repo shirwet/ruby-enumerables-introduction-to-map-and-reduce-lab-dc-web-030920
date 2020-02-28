8da30116a483425aec958b4205e39210d4e8db1f
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-28 22:41:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -53,3 +53,15 @@ while i < source_array.length do
 end
 return total
 end
+
+def reduce_to_total(source_array, starting_point=0)
+
+  total = starting_point
+i = 0
+while i < source_array.length do
+  total += source_array[i]
+  
+  i+=1
+end
+return total
+end
