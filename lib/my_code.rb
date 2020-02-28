fafc808f5e40ef511c215ba310f8ade98fd46c1b
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-28 22:42:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -54,12 +54,12 @@ end
 return total
 end
 
-def reduce_to_total(source_array, starting_point=0)
+def reduce_to_all_true(source_array)
 
-  total = starting_point
+
 i = 0
 while i < source_array.length do
-  total += source_array[i]
+  
   
   i+=1
 end
